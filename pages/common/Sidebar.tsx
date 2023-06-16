9d8fff8dccf2dc746afcf17b390b7b9826572c8f
Eric <eric2umeh@yahoo.com>
2023-06-16 17:05:53 +0100

    Logout function

--- pages/common/Sidebar.tsx
+++ pages/common/Sidebar.tsx
@@ -1,13 +1,76 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+interface TokenData {
+  exp: number;
+}
+
+function decodeToken(token: string): TokenData | null {
+  try {
+    const payload = token.split('.')[1];
+    const decodedData = atob(payload);
+    return JSON.parse(decodedData);
+  } catch (error) {
+    return null;
+  }
+}
 
 const Sidebar = () => {
+  const sidebarData = [
+    {
+      icon: "/img/dashboard/sidebar/canoe.svg",
+      title: "FAQ",
+      links: [
+        { title: "Company", href: "/faq-company" },
+        { title: "User", href: "/faq-user" },
+      ],
+    },
+  ];
+
+  const router = useRouter();
+
+  const handleLogout = () => {
+    localStorage.setItem("token", "");
+    router.push('/login');
+  };
+
+  // Automatically logout token after the expired time (from backend)
+  useEffect(() => {
+    const isTokenExpired = (): boolean => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        return true;
+      }
+
+      const decodedToken = decodeToken(token);
+      if (!decodedToken) {
+        return true;
+      }
+
+      const tokenExpiration = decodedToken.exp;
+      const currentTime = Math.floor(Date.now() / 1000);
+
+      return tokenExpiration < currentTime;
+    };
+
+    const interval = setInterval(() => {
+      if (isTokenExpired()) {
+        handleLogout();
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <>
       <div className="sidebar -dashboard" id="vendorSidebarMenu">
         <div className="sidebar__item ">
           <a
-            href="/dashboard"
+            href="/"
             className="sidebar__button d-flex items-center text-15 lh-1 fw-500"
           >
             <Image
@@ -88,8 +151,8 @@ const Sidebar = () => {
 
         <div className="sidebar__item ">
           <a
-            href="/"
-            className="sidebar__button d-flex items-center text-15 lh-1 fw-500"
+            onClick={handleLogout}
+            className="sidebar__button d-flex items-center text-15 lh-1 fw-500 cursor-pointer"
           >
             <Image
               width={20}
